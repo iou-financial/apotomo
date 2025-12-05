38c27147dc1aeec9e2fa4db4ed4b61d6cd68a78c
Andrew Vogel <andrewpvogel@gmail.com>
2025-12-05 09:09:52 -0700

    fix: prevent unsafe code from being evaled during render_event_response
    Co-authored-by: Andrew Vogel <andrew.vogel@hashrocket.com>
    Co-authored-by: Matt Polito <matt.polito@hashrocket.com>

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,14 @@ Rails.application.routes.draw do
     Rails.logger.error("ROUTE NOT FOUND. USING WILDCARD ROUTE. REQUIRED ROUTE IS:>")
     Rails.logger.error("#{env['REQUEST_METHOD'].downcase} #{route[:controller]}/render_event_response => #{route[:controller]}#render_event_response")
     Rails.logger.error("******************************************************************************************************")
-    controller = eval("#{route[:controller].camelize}Controller")
-    action = 'render_event_response'
-    controller.action(action).call(env)
+
+    controller = "#{route[:controller].camelize}Controller".safe_constantize
+    action = "render_event_response"
+
+    if controller
+      controller.action(action).call(env)
+    else
+      [422, {}, ["Unprocessable entity"]]
+    end
   }
 end
